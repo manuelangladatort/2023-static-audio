103a8911e48fb2c1fccf0d1d6fdfd8a1e5a2f0f3
manuelangladatort <manel.anglada.tort@gmail.com>
2023-10-10 11:50:31 +0100

    first commit

---
+++ experiment.py
@@ -0,0 +1,152 @@
+import random
+from markupsafe import Markup
+
+import psynet.experiment
+from psynet.asset import DebugStorage
+from psynet.consent import NoConsent, MainConsent
+from psynet.modular_page import AudioPrompt, ModularPage, PushButtonControl
+from psynet.page import InfoPage, SuccessfulEndPage, VolumeCalibration
+from psynet.timeline import Timeline, Event
+from psynet.trial import compile_nodes_from_directory
+from psynet.trial.static import StaticNode, StaticTrial, StaticTrialMaker
+from psynet.utils import get_logger
+from psynet.prescreen import AntiphaseHeadphoneTest
+
+
+logger = get_logger()
+
+########################################################################################################################
+# Global params
+########################################################################################################################
+TARGET_NUM_PARTICIPANTS = 10
+
+TRIALS_PER_PARTICIPANT = 5
+TRIALS_PER_PARTICIPANT_PRACTICE = 2
+N_REPEAT_TRIALS = 0
+
+########################################################################################################################
+# Experiment parts
+########################################################################################################################
+def instructions():
+    return InfoPage(
+        Markup(
+            """
+            <h3>Instructions</h3>
+            <hr>
+            In this experiment, you will hear melodies and be asked to sing them back as accurately as possible.
+            <br><br>
+            Press <b><b>next</b></b> when you are ready to start.
+            <hr>
+            """
+        ),
+        time_estimate=0
+    )
+
+
+def requirements():
+    return InfoPage(
+        Markup(
+            """
+            <h3>Requirements</h3>
+            <hr>
+            <b><b>For this experiment we need you to use headphones or earplugs and a working microphone.</b></b>
+            <br><br>
+            However, we ask that you do not wear wireless headphones/earphones (e.g. EarPods),
+            they often introduce recording issues.
+            <br><br>
+            If you cannot satisfy these requirements or your microphone or your headphones/earplugs do not work, please return the survey now.
+            <hr>
+            """
+        ),
+        time_estimate=0
+    )
+
+
+class RatingTrial(StaticTrial):
+    time_estimate = 10
+
+    def show_trial(self, experiment, participant):
+
+        # Debugger
+        # import pydevd_pycharm
+        # pydevd_pycharm.settrace('localhost', port=52218, stdoutToServer=True, stderrToServer=True)
+
+        # count number trials per trial
+        if self.trial_maker_id == "audio_practice":
+            n_trials = TRIALS_PER_PARTICIPANT_PRACTICE
+        else:
+            n_trials = TRIALS_PER_PARTICIPANT + N_REPEAT_TRIALS
+
+        current_trial = self.position + 1
+        show_current_trial = f'<i>Trial number {current_trial} out of {n_trials} trials.</i>'
+
+        return ModularPage(
+            "question_page",
+            AudioPrompt(self.assets["prompt"],
+                        Markup(f"""
+                        <h5>How much do you like the song?</h5>
+                        <i>{show_current_trial}</i>
+                        """
+                               )
+                        ),
+            PushButtonControl(
+                choices=[1, 2, 3, 4, 5, 6, 7],
+                labels=[
+                    "(1) Very unpleasant",
+                    "(2)",
+                    "(3)",
+                    "(4)",
+                    "(5)",
+                    "(6)",
+                    "(7) Very pleasant",
+                ],
+                arrange_vertically=True,
+            ),
+            events={
+                "responseEnable": Event(is_triggered_by="promptEnd"),
+                "submitEnable": Event(is_triggered_by="promptEnd")
+            }
+        )
+
+
+########################################################################################################################
+# Timeline
+########################################################################################################################
+class Exp(psynet.experiment.Experiment):
+    label = "Audio stimulus set from directory demo"
+    asset_storage = DebugStorage()
+
+    timeline = Timeline(
+        NoConsent(),
+        # MainConsent(),  # use for main experiment
+        InfoPage("Welcome page", time_estimate=5),
+        requirements(),
+        # VolumeCalibration(),
+        # AntiphaseHeadphoneTest(),
+        instructions(),
+        StaticTrialMaker(
+            id_="audio_practice",
+            trial_class=RatingTrial,
+            nodes=compile_nodes_from_directory(
+                input_dir="input/practice", media_ext=".mp3", node_class=StaticNode
+            ),
+            target_n_participants=0,
+            recruit_mode="n_participants",
+            expected_trials_per_participant=TRIALS_PER_PARTICIPANT_PRACTICE,
+            max_trials_per_participant=TRIALS_PER_PARTICIPANT_PRACTICE,
+        ),
+        InfoPage("We continue with the experiment trials.", time_estimate=5),
+        StaticTrialMaker(
+            id_="audio_experiment",
+            trial_class=RatingTrial,
+            nodes=compile_nodes_from_directory(
+                input_dir="input/experiment", media_ext=".mp3", node_class=StaticNode
+            ),
+            target_n_participants=TARGET_NUM_PARTICIPANTS,
+            recruit_mode="n_participants",
+            expected_trials_per_participant=TRIALS_PER_PARTICIPANT,
+            max_trials_per_participant=TRIALS_PER_PARTICIPANT,
+        ),
+        # TODO: add questionnaires here
+        SuccessfulEndPage(),
+    )
