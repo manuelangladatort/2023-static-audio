ea3ed6ec273c67daf98b41844372582b8b9aeae5
jzielk <93276692+jzielk@users.noreply.github.com>
2023-12-13 08:18:37 -0800

    Update analysis.R

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -2,6 +2,8 @@ library(tidyverse)
 
 ratings_data = read_csv("~/Documents/github/2023-static-audio/data/anonymous/data/RatingTrial.csv")
 
+participant_data = read_csv("~/Documents/github/2023-static-audio/data/anonymous/data/Participant.csv")
+
 ratings_data_clean = ratings_data %>% 
   filter(failed == FALSE) %>%  # filter out participants who failed
   filter(is_repeat_trial == "FALSE") %>%  # filter out repeated trials
@@ -32,3 +34,19 @@ ggplot(ratings_data_clean, aes(reorder(audio_name, z_answer), z_answer, color=au
 
 ggsave("song_ratings.png", height = 10, width = 20, units = "cm")
   
+participant_data_clean = participant_data %>% 
+  filter(failed == FALSE) %>%  # filter out participants who failed
+  select(id, gender, age, gmsi) %>%
+  rename(participant_id = id) %>%
+  mutate(gmsi_12 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_12": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_2 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_02": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_16 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_16": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_18 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_18": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_21 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_21": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_22 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_22": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  mutate(gmsi_35 = as.numeric(str_extract(str_extract(str_extract(gmsi, '"q_35": [0-9]'), ': [0-9]'), '[0-9]'))) %>%
+  group_by(participant_id) %>%
+  mutate(gmsi_score = rowMeans(cbind(gmsi_12, gmsi_2, gmsi_16, gmsi_18, gmsi_21, gmsi_22, gmsi_35))) %>%
+  select(-gmsi)
+
+data_combined = merge(x = participant_data_clean, y = ratings_data_clean, by = "participant_id", all = TRUE)
